efd7d2d6b55925b5d0d20c2c4b807bd714a3462c
Billy Korando <wkorando@gmail.com>
2019-08-22 11:23:56 -0500

    Making scripts more portable, adding no stubs example

---
+++ run-produce-service-mock-no-stubs.sh
@@ -0,0 +1,9 @@
+docker run  --rm \
+-e "STUBRUNNER_IDS=com.ibm.developer:produce-service:+:stubs:9876" \
+-e "STUBRUNNER_REPOSITORY_ROOT=git://https://github.com/wkorando/produce-contracts.git" \
+-e "STUBRUNNER_STUBS_MODE=REMOTE" \
+-e "STUBRUNNER_GENERATE_STUBS=TRUE" \
+-p "8083:8083" \
+-p "9876:9876" \
+springcloud/spring-cloud-contract-stub-runner:2.2.0.BUILD-SNAPSHOT 
+
--- validate-produce-service.sh
+++ validate-produce-service.sh
@@ -1,9 +1,9 @@
 docker run  --rm \
--e "APPLICATION_BASE_URL=http://192.168.65.2:8080" \
+-e "APPLICATION_BASE_URL=http://host.docker.internal:8080" \
 -e "PUBLISH_ARTIFACTS=true" \
 -e "PROJECT_NAME=produce-service" \
 -e "PROJECT_GROUP=com.ibm.developer" \
--e "REPO_WITH_BINARIES_URL=http://192.168.65.2:8081/artifactory/libs-release-local" \
+-e "REPO_WITH_BINARIES_URL=http://host.docker.internal:8081/artifactory/libs-release-local" \
 -e "PROJECT_VERSION=0.0.1-SNAPSHOT" \
 -e "EXTERNAL_CONTRACTS_GROUP_ID=com.ibm.developer" \
 -e "EXTERNAL_CONTRACTS_ARTIFACT_ID=produce-service" \
@@ -15,3 +15,7 @@ springcloud/spring-cloud-contract:2.1.1.RELEASE
 
 #2.1.1.RELEASE
 #2.2.0.BUILD-SNAPSHOT --no tests
+#-e "EXTERNAL_CONTRACTS_PATH=/path/to/contracts/repo/root" \
+#-e "EXTERNAL_CONTRACTS_REPO_WITH_BINARIES_URL=git://https://github.com/wkorando/produce-contracts.git" \
+#-e "EXTERNAL_CONTRACTS_PATH=/Users/William.Korando@ibm.com/code/collaborative-contract-driven-development-2-0/produce-contracts/" \
+#-e "APPLICATION_BASE_URL=http://host.docker.internal:8080" \
